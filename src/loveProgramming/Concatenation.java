0450961b11a211cd36b0d5268e77df3b476920f9
Jaya Vunna <you@example.com>
2019-01-11 15:10:59 +0000

    dgjglsd

---
+++ src/loveProgramming/Concatenation.java
@@ -0,0 +1,19 @@
+package loveProgramming;
+
+public class Concatenation {
+    public static void main(String[] args)
+    {
+        String myString = "";
+
+            for (int i = 20; i >= 0; i--)
+            {
+                myString = myString + i;
+            }
+
+            System.out.println(myString);
+
+
+
+
+    }
+}
---
+++ src/loveProgramming/Values.java
@@ -0,0 +1,17 @@
+package loveProgramming;
+
+public class Values
+{
+    public static void main(String[] args)
+    {
+        String s1="the";
+        String s2="string";
+        String s3="cocatenation";
+        String s4="works";
+        String add=s1+s2+s3+s4;
+        System.out.println(add);
+
+
+
+    }
+}
